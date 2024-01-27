cbde7435224ceb13f32739674fe11c1130e1b168
Aarjit <poudelaarjit@gmail.com>
2024-01-27 21:22:43 +0545

    add

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
   session({
-    secret: "your-secret-key",
+    secret: "1",
     resave: false,
     saveUninitialized: true,
   })
@@ -26,6 +26,7 @@ app.use(express.static(publicPath));
 app.get("/signup", (req, res) => {
   res.render("signup");
 });
+
 app
   .route("/")
   .all(async (req, res, next) => {
@@ -57,7 +58,7 @@ app
     }
   })
   .get((req, res) => {
-    res.render("login");
+    res.render("main");
   })
   .post((req, res) => {
     req.session.destroy((err) => {
@@ -69,6 +70,7 @@ app
       }
     });
   });
+
 app.get("/", async (req, res) => {
   try {
     if (req.session.loggedIn) {
@@ -97,6 +99,7 @@ app.get("/", async (req, res) => {
     res.status(500).send("Internal Server Error");
   }
 });
+
 app.get("/login", async (req, res) => {
   try {
     if (req.session.loggedIn) {
@@ -125,6 +128,7 @@ app.get("/login", async (req, res) => {
     res.status(500).send("Internal Server Error");
   }
 });
+
 app.get("/signup", async (req, res) => {
   try {
     if (req.session.loggedIn) {
@@ -153,49 +157,6 @@ app.get("/signup", async (req, res) => {
     res.status(500).send("Internal Server Error");
   }
 });
-// app.post("/add-reply", async (req, res) => {
-//   try {
-//     console.log("Reply form submitted by user: " + req.body.name); // Add this line for logging
-
-//     const user = await LogInCollection.findOne({ name: req.body.name });
-//     console.log("User found: " + user);
-//     if (user) {
-//       const reply = await Reply.create({
-//         user: user._id,
-//         question: req.body.questionId,
-//         reply: req.body.reply,
-//       });
-
-//       const question = await CommunityQuestion.findById(req.body.questionId);
-//       question.replies.push(reply);
-//       await question.save();
-
-//       const userQuestions = await LogInCollection.findOne({
-//         name: req.session.username,
-//       })
-//         .populate("questions")
-//         .exec();
-
-//       const communityQuestions = await CommunityQuestion.find().populate(
-//         "user",
-//         "name"
-//       );
-
-//       const allQuestions = userQuestions.questions.concat(communityQuestions);
-
-//       res.status(201).render("home", {
-//         naming: req.body.name,
-//         questions: allQuestions,
-//       });
-//     } else {
-//       res.send("User not found");
-//     }
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send("Internal Server Error");
-//   }
-// });
-
 app.post("/login", async (req, res) => {
   try {
     const user = await LogInCollection.findOne({ name: req.body.name });
@@ -288,7 +249,81 @@ app.post("/add-question", async (req, res) => {
     res.status(500).send("Internal Server Error");
   }
 });
+app.post("/signup", async (req, res) => {
+  try {
+    const { name, password, category } = req.body;
 
+    if (!name || !password || !category) {
+      return res.status(400).send("Name, password, and category are required");
+    }
+
+    const existingUser = await LogInCollection.findOne({ name });
+
+    if (existingUser) {
+      return res.status(409).send("User with that name already exists");
+    }
+
+    const newUser = new LogInCollection({
+      name,
+      password,
+      category,
+    });
+
+    await newUser.save();
+
+    req.session.loggedIn = true;
+    req.session.username = name;
+    req.session.questions = [];
+
+    res.redirect("/"); //redirection code
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
 app.listen(port, () => {
   console.log("Port connected");
 });
+//reply section (not completed)
+// app.post("/add-reply", async (req, res) => {
+//   try {
+//     console.log("Reply form submitted by user: " + req.body.name); // Add this line for logging
+
+//     const user = await LogInCollection.findOne({ name: req.body.name });
+//     console.log("User found: " + user);
+//     if (user) {
+//       const reply = await Reply.create({
+//         user: user._id,
+//         question: req.body.questionId,
+//         reply: req.body.reply,
+//       });
+
+//       const question = await CommunityQuestion.findById(req.body.questionId);
+//       question.replies.push(reply);
+//       await question.save();
+
+//       const userQuestions = await LogInCollection.findOne({
+//         name: req.session.username,
+//       })
+//         .populate("questions")
+//         .exec();
+
+//       const communityQuestions = await CommunityQuestion.find().populate(
+//         "user",
+//         "name"
+//       );
+
+//       const allQuestions = userQuestions.questions.concat(communityQuestions);
+
+//       res.status(201).render("home", {
+//         naming: req.body.name,
+//         questions: allQuestions,
+//       });
+//     } else {
+//       res.send("User not found");
+//     }
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send("Internal Server Error");
+//   }
+// });
